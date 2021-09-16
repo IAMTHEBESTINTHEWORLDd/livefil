05a7eaeae54b6012a8921920a23bb19b4ab164a8
IAMTHEBESTINTHEWORLDd <90430931+IAMTHEBESTINTHEWORLDd@users.noreply.github.com>
2021-09-15 17:16:16 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-setup()
+function setup()
 {
-    canvas=createCanvas(300,300);
+    canvas=createCanvas(455,150);
     canvas.position(455,150);
-};
+}
 
 function savee(){
     savee("live picture")
 }
-
-
