29d2bbc7b620293827bf43babb1f648652443167
IAMTHEBESTINTHEWORLDd <90430931+IAMTHEBESTINTHEWORLDd@users.noreply.github.com>
2021-09-22 17:21:54 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
 function setup()
 {
     canvas=createCanvas(455,355);
-    canvas.position(420,150);
+    canvas.position(105,150);
+
+    chimera=createCapture(VIDEO);
+    chimera.hide()
+    posinet=ml5.poseNet(chimera, MODALLODAD);
+    posinet.on('pose', GOTDAPOSES);
 }
+
+function GOTDAPOSES(results){
+if(results.length>0){
+    console.log(results);
+    console.log("NOSY X="+results[0].pose.nose.x);
+    console.log("NOSY Y="+results[0].pose.nose.y);
+    Nose_x=results[0].pose.nose.x;
+    Nose_y=results[0].pose.nose.y;
+}
+}
+
+ function MODALLODAD(){
+     console.log("THE ML5 FOR THE MESSAGE THAT THE POSENET IS LODED: "+ml5.version);
+ }
+
+
+function draw(){
+    image(chimera, 0, 0, 500, 400);
+}
+
 
 function savee(){
     savee("live picture")
